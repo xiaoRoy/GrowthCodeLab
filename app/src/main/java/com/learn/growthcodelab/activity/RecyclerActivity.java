eb8c42aa11b8e94fe0d892e8743d74a3d5f5081f
Jerry Li <jerry@Jerrys-MacBook-Pro.local>
2017-08-27 21:49:23 +0800

    Finished initlization recycler.

---
+++ app/src/main/java/com/learn/growthcodelab/activity/RecyclerActivity.java
@@ -0,0 +1,33 @@
+package com.learn.growthcodelab.activity;
+
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import com.learn.growthcodelab.R;
+import com.learn.growthcodelab.recycler.TextAdapter;
+
+public class RecyclerActivity extends BaseActivity {
+
+    private RecyclerView mRecyclerText;
+
+    private TextAdapter mTextAdapter;
+
+    public static void start(Context context){
+        context.startActivity(new Intent(context, RecyclerActivity.class));
+    }
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recycler);
+        mRecyclerText = (RecyclerView) findViewById(R.id.recycler_text);
+        mTextAdapter = new TextAdapter();
+        mRecyclerText.setLayoutManager(new LinearLayoutManager(this));
+        mRecyclerText.setAdapter(mTextAdapter);
+    }
+}
---
+++ app/src/main/java/com/learn/growthcodelab/recycler/TextAdapter.java
@@ -0,0 +1,40 @@
+package com.learn.growthcodelab.recycler;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.learn.growthcodelab.R;
+
+public class TextAdapter extends RecyclerView.Adapter<TextAdapter.TextViewHolder> {
+
+    @Override
+    public TextViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.layout_item_text, parent, false);
+        TextViewHolder viewHolder = new TextViewHolder(itemView);
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(TextViewHolder holder, int position) {
+        String text = holder.itemView.getResources().getString(R.string.s_recycler_item_text, position);
+        holder.tvText.setText(text);
+    }
+
+    @Override
+    public int getItemCount() {
+        return 128;
+    }
+
+    public static class TextViewHolder extends RecyclerView.ViewHolder{
+
+        public TextView tvText;
+
+        public TextViewHolder(View itemView) {
+            super(itemView);
+            tvText = (TextView)itemView.findViewById(R.id.tv_text);
+        }
+    }
+}
