d20c7bd268bdc596b75dc7b75f2d0953649f9caa
Jerry Li <xiaoroyg@163.com>
2017-12-12 22:32:55 +0800

    Fihished infinite view pager.

---
+++ app/src/main/java/com/learn/growthcodelab/activity/ViewPagerActivity.java
@@ -0,0 +1,69 @@
+package com.learn.growthcodelab.activity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.view.PagerAdapter;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.learn.growthcodelab.R;
+import com.learn.growthcodelab.view.viewpager.InfiniteViewPager;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ViewPagerActivity extends BaseActivity {
+
+    public static void start(Context context){
+        context.startActivity(new Intent(context, ViewPagerActivity.class));
+    }
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_pager);
+        InfiniteViewPager infiniteViewPager = (InfiniteViewPager) findViewById(R.id.view_pager);
+        List<Integer> colorIntList = new ArrayList<>();
+        colorIntList.add(ContextCompat.getColor(this, android.R.color.holo_red_dark));
+        colorIntList.add(ContextCompat.getColor(this, android.R.color.holo_blue_dark));
+        colorIntList.add(ContextCompat.getColor(this, android.R.color.holo_green_light));
+        colorIntList.add(ContextCompat.getColor(this, android.R.color.holo_orange_dark));
+        infiniteViewPager.setAdapter(new SimpleViewPagerAdapter(colorIntList));
+    }
+
+    private static class SimpleViewPagerAdapter extends PagerAdapter{
+
+        final List<Integer> mColorIntList;
+
+        public SimpleViewPagerAdapter(List<Integer> colorIntList) {
+            mColorIntList = colorIntList;
+        }
+
+        @Override
+        public int getCount() {
+            return mColorIntList.size();
+        }
+
+        @Override
+        public boolean isViewFromObject(View view, Object object) {
+            return view == object;
+        }
+
+        @Override
+        public Object instantiateItem(ViewGroup container, int position) {
+            View item = LayoutInflater.from(container.getContext()).inflate(R.layout.view_pager_item_color, container, false);
+            item.setBackgroundColor(mColorIntList.get(position));
+            container.addView(item);
+            return item;
+        }
+
+        @Override
+        public void destroyItem(ViewGroup container, int position, Object object) {
+            container.removeView((View)object);
+        }
+    }
+}
---
+++ app/src/main/java/com/learn/growthcodelab/view/viewpager/InfiniteViewPagerAdapter.java
@@ -0,0 +1,136 @@
+package com.learn.growthcodelab.view.viewpager;
+
+import android.os.Parcelable;
+import android.support.v4.view.PagerAdapter;
+import android.util.SparseArray;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class InfiniteViewPagerAdapter extends PagerAdapter {
+
+    final private PagerAdapter mPagerAdapter;
+
+    private boolean mHasCaching;
+
+    private SparseArray<Boundary> mBoundarySparseArray = new SparseArray<>();
+
+    public InfiniteViewPagerAdapter(PagerAdapter pagerAdapter) {
+        mPagerAdapter = pagerAdapter;
+    }
+
+    public PagerAdapter getPagerAdapter() {
+        return mPagerAdapter;
+    }
+
+    public void setHasCaching(boolean hasCaching) {
+        mHasCaching = hasCaching;
+    }
+
+    @Override
+    public int getCount() {
+        return mPagerAdapter.getCount() + 2;
+    }
+
+    public int getExactCount(){
+        return mPagerAdapter.getCount();
+    }
+
+    int calculateExactPosition(int adapterPosition){
+        int exactCount = getExactCount();
+        int exactPosition = 0;
+        if(exactCount > 0){
+            exactPosition = (adapterPosition - 1) % exactCount;
+        }
+        if(exactPosition < 0){
+            exactPosition += exactCount;
+        }
+        return exactPosition;
+    }
+
+    public int calculateAdapterPosition(int exactPosition){
+        return exactPosition + 1;
+    }
+
+    private int getExactFirstAdapterPosition(){
+        return 1;
+    }
+
+    private int getExactLastAdapterPosition(){
+        return getExactCount();
+    }
+
+    @Override
+    public Object instantiateItem(ViewGroup container, int position) {
+        Object item;
+        Boundary boundary = mBoundarySparseArray.get(position);
+        if(mHasCaching && boundary != null){
+            mBoundarySparseArray.remove(position);
+            item = boundary.object;
+        } else {
+            int exactionPosition =  calculateExactPosition(position);
+            item = mPagerAdapter.instantiateItem(container, exactionPosition);
+        }
+        return item;
+    }
+
+    @Override
+    public void destroyItem(ViewGroup container, int position, Object object) {
+        int exactFirstAdapterPosition = getExactFirstAdapterPosition();
+        int exactLastAdapterPosition = getExactLastAdapterPosition();
+        int exactPosition = calculateExactPosition(position);
+        if(mHasCaching && (exactFirstAdapterPosition == exactPosition || exactLastAdapterPosition == exactPosition)){
+            mBoundarySparseArray.put(position, new Boundary(container, exactPosition, object));
+        } else {
+            mPagerAdapter.destroyItem(container, position, object);
+        }
+    }
+
+    @Override
+    public boolean isViewFromObject(View view, Object object) {
+        return mPagerAdapter.isViewFromObject(view, object);
+    }
+
+    @Override
+    public void startUpdate(ViewGroup container) {
+        mPagerAdapter.startUpdate(container);
+    }
+
+    @Override
+    public void finishUpdate(ViewGroup container) {
+        mPagerAdapter.finishUpdate(container);
+    }
+
+    @Override
+    public Parcelable saveState() {
+        return mPagerAdapter.saveState();
+    }
+
+    @Override
+    public void restoreState(Parcelable state, ClassLoader loader) {
+        mPagerAdapter.restoreState(state, loader);
+    }
+
+    @Override
+    public void setPrimaryItem(ViewGroup container, int position, Object object) {
+        mPagerAdapter.setPrimaryItem(container, position, object);
+    }
+
+    @Override
+    public void notifyDataSetChanged() {
+        mBoundarySparseArray.clear();
+        super.notifyDataSetChanged();
+    }
+
+    static class Boundary {
+        ViewGroup container;
+        int position;
+        Object object;
+
+        public Boundary(ViewGroup container, int position, Object object) {
+            this.container = container;
+            this.position = position;
+            this.object = object;
+        }
+    }
+}
