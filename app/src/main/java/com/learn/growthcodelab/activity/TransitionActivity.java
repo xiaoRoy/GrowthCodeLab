c6a4d6413557d4dcd48362a0d9807e93569d5fae
Jerry Li <jerli@xogrp.com>
2018-01-05 18:14:06 +0800

    Learn scene transition.

--- app/src/main/java/com/learn/growthcodelab/activity/TransitionActivity.java
+++ app/src/main/java/com/learn/growthcodelab/activity/TransitionActivity.java
@@ -4,26 +4,23 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v7.app.AppCompatActivity;
 
 import com.learn.growthcodelab.R;
-import com.learn.growthcodelab.playground.SceneTransitionFragment;
+import com.learn.growthcodelab.scenetransition.ScenesFragment;
 
-
-public class PlayGroundActivity extends AppCompatActivity {
+public class TransitionActivity extends BaseActivity {
 
     public static void start(Context context){
-        context.startActivity(new Intent(context, PlayGroundActivity.class));
+        context.startActivity(new Intent(context, TransitionActivity.class));
     }
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_play_ground);
+        setContentView(R.layout.activity_transition);
         getSupportFragmentManager()
                 .beginTransaction()
-                .add(R.id.fl_play_ground_container, SceneTransitionFragment.newInstance(), "LayoutTransition")
-                .addToBackStack(null)
+                .add(R.id.fl_transition_container, ScenesFragment.newInstance())
                 .commit();
     }
 }
---
+++ app/src/main/java/com/learn/growthcodelab/playground/SceneTransitionFragment.java
@@ -0,0 +1,103 @@
+package com.learn.growthcodelab.playground;
+
+import android.animation.Animator;
+import android.animation.ObjectAnimator;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.transition.Fade;
+import android.support.transition.Scene;
+import android.support.transition.Transition;
+import android.support.transition.TransitionManager;
+import android.support.transition.TransitionValues;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
+
+import com.learn.growthcodelab.R;
+import com.learn.growthcodelab.fragment.BaseFragment;
+
+
+public class SceneTransitionFragment extends BaseFragment implements View.OnClickListener {
+
+    private View mViewCardFirst, mViewCardSecond;
+    private Scene mFirstScene, mSecondScene;
+    private Fade mFade;
+    private FrameLayout mFlCardContainer;
+
+    public static SceneTransitionFragment newInstance() {
+        return new SceneTransitionFragment();
+    }
+
+    public SceneTransitionFragment() {
+    }
+
+    @Override
+    public int getLayoutRes() {
+        return R.layout.fragment_layout_transition;
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        mViewCardFirst = view.findViewById(R.id.card_first);
+        mViewCardSecond = view.findViewById(R.id.card_second);
+        mFlCardContainer = (FrameLayout) view.findViewById(R.id.fl_card_container);
+        view.findViewById(R.id.btn_layout_transition_change).setOnClickListener(this);
+        mFirstScene = Scene.getSceneForLayout(mFlCardContainer, R.layout.layout_first_card, getActivity());
+        mSecondScene = Scene.getSceneForLayout(mFlCardContainer, R.layout.layout_second_card, getActivity());
+    }
+
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.btn_layout_transition_change:
+                TransitionManager.go(mSecondScene, new SlideTransition());
+                break;
+        }
+    }
+
+    private static void toggleVisibility(View view) {
+        if (view.getVisibility() == View.GONE) {
+            view.setVisibility(View.VISIBLE);
+        } else {
+            view.setVisibility(View.GONE);
+        }
+    }
+
+    private static class SlideTransition extends Transition {
+
+        private static final String PROPERTY_NAME_TRANSLATION_X = "com.learn.growthcodelab:SlideTransition:x";
+
+        @Override
+        public void captureEndValues(@NonNull TransitionValues transitionValues) {
+            System.out.println("trail.captureEndValues");
+            View view = transitionValues.view;
+            if (view.getId() == R.id.card_second) {
+                transitionValues.values.put(PROPERTY_NAME_TRANSLATION_X, 0f);
+            }
+        }
+
+        @Override
+        public void captureStartValues(@NonNull TransitionValues transitionValues) {
+            System.out.println("trail.captureStartValues");
+            View view = transitionValues.view;
+            if (view.getId() == R.id.card_second) {
+                transitionValues.values.put(PROPERTY_NAME_TRANSLATION_X, view.getWidth() + 0.0f);
+            }
+        }
+
+        @Nullable
+        @Override
+        public Animator createAnimator(@NonNull ViewGroup sceneRoot, @Nullable TransitionValues startValues, @Nullable TransitionValues endValues) {
+            System.out.println("trail.createAnimator");
+            if (startValues != null && endValues != null) {
+                Animator animator = ObjectAnimator.ofFloat(endValues.view, "translationX", (Float) startValues.values.get(PROPERTY_NAME_TRANSLATION_X), (Float) endValues.values.get(PROPERTY_NAME_TRANSLATION_X));
+                return animator.setDuration(800);
+            } else {
+                return super.createAnimator(sceneRoot, startValues, endValues);
+            }
+        }
+    }
+}
