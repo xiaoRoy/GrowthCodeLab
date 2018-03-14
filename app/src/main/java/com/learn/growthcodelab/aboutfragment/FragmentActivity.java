002667eec37a4d6aa4359c28a299362b58394af6
Jerry Li <xiaoroyg@163.com>
2018-03-14 22:23:18 +0800

    Update gradle plugin.

---
+++ app/src/main/java/com/learn/growthcodelab/aboutfragment/FragmentActivity.java
@@ -0,0 +1,16 @@
+package com.learn.growthcodelab.aboutfragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import com.learn.growthcodelab.activity.BaseActivity;
+
+
+public class FragmentActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
