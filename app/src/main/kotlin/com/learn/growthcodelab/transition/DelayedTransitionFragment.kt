f5524203c25f41fec0639427240b44193f96d51e
Jerry Li <xiaoroyg@163.com>
2018-09-11 23:33:17 +0800

    Learned scene animate view layout element.

---
+++ app/src/main/kotlin/com/learn/growthcodelab/transition/DelayedTransitionFragment.kt
@@ -0,0 +1,48 @@
+package com.learn.growthcodelab.transition
+
+import android.os.Bundle
+import android.transition.TransitionManager
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.learn.growthcodelab.R
+import com.learn.growthcodelab.databinding.FragmentDelayedTransitionBinding
+import com.learn.growthcodelab.fragment.BaseFragment
+
+class DelayedTransitionFragment : BaseFragment(), DelayedTransitionViewModel.OnDelayedTransitionClickListener {
+
+    companion object {
+        fun newInstance() = DelayedTransitionFragment()
+    }
+
+    private lateinit var binding: FragmentDelayedTransitionBinding
+
+    private var isCircleSizeChanged: Boolean = false
+    private var circleSavedSize = 0
+
+    override fun getLayoutRes() = R.layout.fragment_delayed_transition
+
+    override fun bindView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        inflater?.let {
+            binding = FragmentDelayedTransitionBinding.bind(it.inflate(layoutRes, container, false))
+            binding.onDelayedTransitionClickListener = this
+        }
+        return binding.root
+    }
+
+    override fun onChangSizeClicked() {
+        TransitionManager.beginDelayedTransition(binding.layoutDelayedTransitionContainer)
+        val layoutParams = binding.ivTransitionCircle.layoutParams
+        if(isCircleSizeChanged){
+            layoutParams.width = circleSavedSize
+            layoutParams.height = circleSavedSize
+        } else {
+            circleSavedSize = layoutParams.width
+            val circleLargeSize = resources.getDimensionPixelOffset(R.dimen.circle_larger_size)
+            layoutParams.width = circleLargeSize
+            layoutParams.height = circleLargeSize
+        }
+        isCircleSizeChanged = !isCircleSizeChanged
+        binding.ivTransitionCircle.layoutParams = layoutParams
+    }
+}
