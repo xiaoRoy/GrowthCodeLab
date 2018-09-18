183344a3bbcef3285133f6fd5c2af2d4c2488659
Jerry Li <xiaoroyg@163.com>
2018-09-18 23:56:17 +0800

    Create scene from code.

---
+++ app/src/main/kotlin/com/learn/growthcodelab/transition/SceneFromCodeFragment.kt
@@ -0,0 +1,15 @@
+package com.learn.growthcodelab.transition
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.LinearLayout
+import com.learn.growthcodelab.fragment.BaseFragment
+
+class SceneFromCodeFragment : BaseFragment() {
+
+    override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
