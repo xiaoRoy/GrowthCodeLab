c7386a72c28c475a11c2ee672f2b5bc05991800f
Jerry Li <xiaoroyg@163.com>
2018-09-19 23:51:13 +0800

    Initialize the DataBindingViewHolder.

---
+++ app/src/main/kotlin/com/learn/growthcodelab/recycler/DataBindingViewHolder.kt
@@ -0,0 +1,11 @@
+package com.learn.growthcodelab.recycler
+
+import android.support.v7.widget.RecyclerView
+import android.view.View
+
+class DataBindingViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView) {
+
+    init {
+
+    }
+}
