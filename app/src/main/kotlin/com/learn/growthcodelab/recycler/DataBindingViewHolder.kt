98644aaac68677cfe96cf2c963f2fac121374faa
Jerry Li <xiaoroyg@163.com>
2018-09-20 23:48:32 +0800

    Finished BaseDataBindingAdapter.

--- app/src/main/kotlin/com/learn/growthcodelab/recycler/DataBindingViewHolder.kt
+++ app/src/main/kotlin/com/learn/growthcodelab/recycler/DataBindingViewHolder.kt
@@ -1,11 +1,14 @@
 package com.learn.growthcodelab.recycler
 
+import android.databinding.ViewDataBinding
 import android.support.v7.widget.RecyclerView
-import android.view.View
+import com.learn.growthcodelab.BR
 
-class DataBindingViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView) {
+class DataBindingViewHolder(val binding: ViewDataBinding) : RecyclerView.ViewHolder(binding.root) {
 
-    init {
-
+    fun bind(item : Any){
+//        binding.setVariable(BR.item, item)
+//        binding.setVariable(BR.adapterPosition, adapterPosition)
+        binding.executePendingBindings()
     }
 }
