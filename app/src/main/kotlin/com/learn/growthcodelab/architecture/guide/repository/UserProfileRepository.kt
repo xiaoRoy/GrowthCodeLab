d60e1aa6b990ad46bb4ead5d755aee84c0af284a
Jerry Li <xiaoroyg@163.com>
2018-11-10 23:33:10 +0800

    Finished setup the live date and repository

---
+++ app/src/main/kotlin/com/learn/growthcodelab/architecture/guide/repository/UserProfileRepository.kt
@@ -0,0 +1,21 @@
+package com.learn.growthcodelab.architecture.guide.repository
+
+import android.arch.lifecycle.LiveData
+import android.arch.lifecycle.MutableLiveData
+import com.learn.growthcodelab.architecture.guide.model.User
+
+class UserProfileRepository {
+
+    private companion object {
+        private val userMock = User("123", "Smith")
+    }
+
+    /*
+    * Mock fetch the user from the remote
+    * */
+    fun getUser() : LiveData<User> {
+        val user = MutableLiveData<User>()
+        user.value = userMock
+        return user
+    }
+}
--- app/src/main/kotlin/com/learn/growthcodelab/architecture/guide/ui/UserProfileFragment.kt
+++ app/src/main/kotlin/com/learn/growthcodelab/architecture/guide/ui/UserProfileFragment.kt
@@ -1,10 +1,14 @@
 package com.learn.growthcodelab.architecture.guide.ui
 
+import android.arch.lifecycle.Observer
+import android.arch.lifecycle.ViewModelProviders
 import android.databinding.DataBindingUtil
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.learn.growthcodelab.architecture.guide.model.User
+import com.learn.growthcodelab.architecture.guide.viewmodel.UserProfileViewModel
 import com.learn.growthcodelab.databinding.FragmentUserProfileBinding
 import com.learn.growthcodelab.fragment.BaseFragment
 
@@ -14,10 +18,19 @@ class UserProfileFragment : BaseFragment() {
         private const val UID_KEY = "uid"
     }
 
+    private lateinit var userProfileViewModel: UserProfileViewModel
+
 
     override fun bindView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View {
-        val binding: FragmentUserProfileBinding
-                = DataBindingUtil.inflate(layoutInflater, layoutRes, container, false)
+        val binding: FragmentUserProfileBinding = DataBindingUtil.inflate(layoutInflater, layoutRes, container, false)
         return binding.root
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        userProfileViewModel = ViewModelProviders.of(this).get(UserProfileViewModel::class.java)
+        userProfileViewModel.getUser().observe(this, Observer<User> {
+            TODO("update UI")
+        })
     }
 }
