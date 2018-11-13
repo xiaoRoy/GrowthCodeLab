a1bedd4afef66ecc127924c61ad0e85decbaa95c
Jerry Li <xiaoroyg@163.com>
2018-11-13 23:58:29 +0800

    Added user cache

--- app/src/main/kotlin/com/learn/growthcodelab/architecture/guide/repository/UserProfileRepository.kt
+++ app/src/main/kotlin/com/learn/growthcodelab/architecture/guide/repository/UserProfileRepository.kt
@@ -4,18 +4,22 @@ import android.arch.lifecycle.LiveData
 import android.arch.lifecycle.MutableLiveData
 import com.learn.growthcodelab.architecture.guide.model.User
 
-class UserProfileRepository {
+class UserProfileRepository(private val userCache: UserCache) {
 
     private companion object {
         private val userMock = User("123", "Smith")
     }
 
-    /*
-    * Mock fetch the user from the remote
-    * */
+
     fun getUser(userId: String) : LiveData<User> {
-        val user = MutableLiveData<User>()
-        user.value = userMock
+        var user = MutableLiveData<User>()
+        if (userCache.isEmpty(userId)) {
+            //Mock fetch the user from the remote, not handle the api error.
+            user.value = userMock
+            userCache.cache(userId, user)
+        } else {
+            user = userCache.get(userId)
+        }
         return user
     }
 }
