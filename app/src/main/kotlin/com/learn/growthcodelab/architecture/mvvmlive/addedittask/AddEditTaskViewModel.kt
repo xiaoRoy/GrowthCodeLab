9743814507fac57c8c16852ed6363812bb2edf3f
Jerry Li <xiaoroyg@163.com>
2018-11-15 23:50:16 +0800

    Rename to AddEditTaskViewModel

---
+++ app/src/main/kotlin/com/learn/growthcodelab/architecture/mvvmlive/addedittask/AddEditTaskViewModel.kt
@@ -0,0 +1,7 @@
+package com.learn.growthcodelab.architecture.mvvmlive.addedittask
+
+import android.app.Application
+import android.arch.lifecycle.AndroidViewModel
+
+class AddEditTaskViewModel(application: Application) : AndroidViewModel(application){
+}
--- app/src/main/kotlin/com/learn/growthcodelab/architecture/mvvmlive/tasks/TaskViewModel.kt
+++
@@ -1,7 +0,0 @@
-package com.learn.growthcodelab.architecture.mvvmlive.tasks
-
-import android.app.Application
-import android.arch.lifecycle.AndroidViewModel
-
-class TaskViewModel(application: Application) : AndroidViewModel(application){
-}
