9589e9a96c2e4c72edea540af062c989e40f5034
Jerry Li <xiaoroyg@163.com>
2018-11-21 23:32:57 +0800

    Created the database.

--- app/src/main/kotlin/com/learn/growthcodelab/architecture/jetpack/word/data/WordDataSource.kt
+++ app/src/main/kotlin/com/learn/growthcodelab/architecture/jetpack/word/data/WordDataSource.kt
@@ -1,12 +1,15 @@
 package com.learn.growthcodelab.architecture.jetpack.word.data
 
+import android.arch.lifecycle.LiveData
 import com.learn.growthcodelab.architecture.jetpack.word.model.Word
 import io.reactivex.Completable
 import io.reactivex.Single
 
 interface WordDataSource {
 
-    fun getAllWords(): Single<List<Word>>
+    fun getAllWords(): LiveData<List<Word>>
+
+    fun getAllWordsList(): List<Word>
 
     fun addWord(word: Word): Completable
 }
