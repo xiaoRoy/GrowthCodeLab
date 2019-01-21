b261f978caabc26cb6dcf1bf8015522cbf3779c6
Jerry Li <xiaoroyg@163.com>
2019-01-21 23:36:01 +0800

    Finished show the product list.

--- app/src/main/java/com/learn/growthcodelab/activity/PlayGroundActivity.java
+++ app/src/main/java/com/learn/growthcodelab/activity/PlayGroundActivity.java
@@ -13,11 +13,12 @@ import com.learn.growthcodelab.databinding.twoway.ViewAttributeFragment;
 import com.learn.growthcodelab.playground.OpenContactsFragment;
 import com.learn.growthcodelab.playground.SceneTransitionFragment;
 import com.learn.growthcodelab.playground.ViewAnimatorFragment;
+import com.learn.growthcodelab.recycler.loadmore.ProductListFragment;
 
 
 public class PlayGroundActivity extends AppCompatActivity {
 
-    public static void start(Context context){
+    public static void start(Context context) {
         context.startActivity(new Intent(context, PlayGroundActivity.class));
     }
 
@@ -27,7 +28,9 @@ public class PlayGroundActivity extends AppCompatActivity {
         setContentView(R.layout.activity_play_ground);
         getSupportFragmentManager()
                 .beginTransaction()
-                .add(R.id.fl_play_ground_container, ViewAttributeFragment.Companion.newInstance(),  "View attribute in data binding")
+                .add(R.id.fl_play_ground_container,
+                        ProductListFragment.Companion.newInstance(),
+                        "Product List")
                 .addToBackStack(null)
                 .commit();
     }
