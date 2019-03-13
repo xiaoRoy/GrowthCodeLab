ca8e319a4d750e91253dabcbb17787fe9fb1b048
Jerry Li <jerli@xogrp.com>
2019-03-13 19:28:35 +0800

    Added OnApplyWindowInsetsListener

--- app/src/main/kotlin/com/learn/growthcodelab/window/drawer/DrawerActivity.kt
+++ app/src/main/kotlin/com/learn/growthcodelab/window/drawer/DrawerActivity.kt
@@ -3,10 +3,14 @@ package com.learn.growthcodelab.window.drawer
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import com.learn.growthcodelab.R
 import com.learn.growthcodelab.activity.BaseActivity
+import com.learn.growthcodelab.databinding.ActivityDrawerBinding
 
-class DrawerActivity : BaseActivity() {
+class DrawerActivity : BaseActivity(), DrawerNavigator {
 
     companion object {
         fun start(context: Context) {
@@ -14,8 +18,49 @@ class DrawerActivity : BaseActivity() {
         }
     }
 
+    private lateinit var binding: ActivityDrawerBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_drawer)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_drawer)
+        binding.flDrawerContent.setOnApplyWindowInsetsListener { view, insets ->
+            var consumed = false
+            (view as ViewGroup).run {
+                for (index in 0 until childCount) {
+                    val child = getChildAt(index)
+                    val childResult = child.dispatchApplyWindowInsets(insets)
+                    if (childResult.isConsumed) {
+                        consumed = true
+                    }
+                    if (child.id == R.id.fl_container_drawer_a) {
+                        Log.d("trail", "drawer a")
+                        Log.d("trail", "drawer a.consumed:$consumed")
+                    }
+                }
+            }
+             Log.d("trail", "drawer a, left:${insets.systemWindowInsetLeft}")
+             Log.d("trail", "drawer a, top:${insets.systemWindowInsetTop}")
+             Log.d("trail", "drawer a, right:${insets.systemWindowInsetRight}")
+             Log.d("trail", "drawer a, bottom:${insets.stableInsetBottom}")
+            if (consumed) insets.consumeSystemWindowInsets() else insets
+        }
+        if(savedInstanceState == null) {
+            supportFragmentManager
+                    .beginTransaction()
+                    .add(R.id.fl_drawer_content, DrawerAFragment.newInstance(), "drawer a")
+                    .commit()
+        }
+    }
+
+    override fun navigateToDrawerB() {
+         supportFragmentManager
+                    .beginTransaction()
+                    .add(R.id.fl_drawer_content, DrawerBFragment.newInstance(), "drawer B")
+                    .addToBackStack("add to B")
+                    .commit()
+    }
+
+    override fun backToDrawerA() {
+        supportFragmentManager.popBackStack()
     }
 }
---
+++ app/src/main/kotlin/com/learn/growthcodelab/window/drawer/DrawerBFragment.kt
@@ -0,0 +1,32 @@
+package com.learn.growthcodelab.window.drawer
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.view.ViewCompat
+import androidx.databinding.DataBindingUtil
+import com.learn.growthcodelab.R
+import com.learn.growthcodelab.databinding.FragmentDrawerBBinding
+
+class DrawerBFragment : BaseDrawerFragment() {
+
+    private lateinit var binding: FragmentDrawerBBinding
+
+    companion object {
+        fun newInstance() = DrawerBFragment()
+    }
+
+    override fun getLayoutRes() = R.layout.fragment_drawer_b
+
+    override fun bindView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        binding = DataBindingUtil.inflate(inflater, layoutRes, container, false)
+        binding.navigator = navigator
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        ViewCompat.requestApplyInsets(view)
+    }
+}
