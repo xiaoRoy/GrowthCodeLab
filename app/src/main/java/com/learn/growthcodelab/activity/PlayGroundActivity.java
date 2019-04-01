91df26f55e399e46063bfc82f9fe47e6cceeee5c
Jerry Li <jerli@xogrp.com>
2019-04-01 14:51:06 +0800

    Move frame metrics to paly ground

--- app/src/main/java/com/learn/growthcodelab/activity/PlayGroundActivity.java
+++ app/src/main/java/com/learn/growthcodelab/activity/PlayGroundActivity.java
@@ -2,15 +2,38 @@ package com.learn.growthcodelab.activity;
 
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
+import android.view.FrameMetrics;
+import android.view.Window;
+
 import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.learn.growthcodelab.R;
 import com.learn.growthcodelab.viewshowcase.constraint.AddViewToConstraintFragment;
 
+import java.util.concurrent.TimeUnit;
+
 
 public class PlayGroundActivity extends AppCompatActivity {
+
+     private Handler frameMetricsHandler = new Handler();
+
+    @RequiresApi(Build.VERSION_CODES.N)
+    private  Window.OnFrameMetricsAvailableListener onFrameMetricsAvailableListener = new Window.OnFrameMetricsAvailableListener() {
+        @Override
+        public void onFrameMetricsAvailable(Window window, FrameMetrics frameMetrics, int dropCountSinceLastInvocation) {
+            FrameMetrics frameMetricsCopy = new FrameMetrics(frameMetrics);
+            long measureDuration = frameMetricsCopy.getMetric(FrameMetrics.LAYOUT_MEASURE_DURATION);
+            long totalDuration = frameMetricsCopy.getMetric(FrameMetrics.TOTAL_DURATION);
+            Log.d("Metrics", "LAYOUT_MEASURE_DURATION:" + TimeUnit.NANOSECONDS.toMillis(measureDuration));
+            Log.d("Metrics", "TOTAL_DURATION:" + TimeUnit.NANOSECONDS.toMillis(totalDuration));
+        }
+    };
 
     public static void start(Context context) {
         context.startActivity(new Intent(context, PlayGroundActivity.class));
@@ -27,5 +50,29 @@ public class PlayGroundActivity extends AppCompatActivity {
                         "add view to constraint")
                 .addToBackStack(null)
                 .commit();
+    }
+
+      @Override
+    protected void onResume() {
+        super.onResume();
+        addOnFrameMetricsAvailableListener();
+    }
+
+    private void addOnFrameMetricsAvailableListener() {
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            getWindow().addOnFrameMetricsAvailableListener(onFrameMetricsAvailableListener, frameMetricsHandler);
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        removeOnFrameMetricsAvailableListener();
+        super.onPause();
+    }
+
+     private void removeOnFrameMetricsAvailableListener() {
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            getWindow().removeOnFrameMetricsAvailableListener(onFrameMetricsAvailableListener);
+        }
     }
 }
