cf30789444de695e87c47450cd5e46a7c7c85d77
Jerry Li <xiaoroyg@163.com>
2019-04-02 23:09:55 +0800

    Finished MeasureLayoutAsyncTask.

--- app/src/main/kotlin/com/learn/growthcodelab/performance/FrameMetricsActivity.kt
+++ app/src/main/kotlin/com/learn/growthcodelab/performance/FrameMetricsActivity.kt
@@ -1,44 +1,8 @@
 package com.learn.growthcodelab.performance
 
-import android.os.AsyncTask
-import android.view.ViewGroup
-import android.widget.Button
-import android.widget.TextView
 import com.learn.growthcodelab.activity.BaseActivity
-import java.lang.ref.WeakReference
-import java.util.concurrent.TimeUnit
 
 class FrameMetricsActivity : BaseActivity(){
 
 
-    private class MeasureLayoutAsyncTask(
-        val executingNthIteration: String,
-        val btnStartRef: WeakReference<Button>,
-        val tvFinishRef: WeakReference<TextView>,
-        val containerRef: WeakReference<ViewGroup>
-    ) : AsyncTask<Void?, Int, Void?>() {
-
-
-        override fun doInBackground(vararg params: Void?): Void? {
-            for(index in 0 until TOTAL) {
-                publishProgress(index)
-                try {
-                    TimeUnit.MILLISECONDS.sleep(100)
-                } catch (ignore: InterruptedException) {
-                    //no op
-                }
-            }
-            return null
-        }
-
-        override fun onProgressUpdate(vararg values: Int?) {
-            val btnStart = btnStartRef.get() ?: return
-            btnStart.text = String.format(executingNthIteration, values[0], TOTAL)
-            val container = containerRef.get() ?: return
-        }
-    }
-
-    companion object {
-        private const val TOTAL = 100
-    }
 }
