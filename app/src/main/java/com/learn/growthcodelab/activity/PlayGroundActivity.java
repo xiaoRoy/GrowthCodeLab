de637ea6b8c6a83008bbca7c95f8a5b01d0a2cdd
Jerry Li <jerli@xogrp.com>
2019-04-09 09:09:31 +0800

    log the frame metrics

--- app/src/main/java/com/learn/growthcodelab/activity/PlayGroundActivity.java
+++ app/src/main/java/com/learn/growthcodelab/activity/PlayGroundActivity.java
@@ -14,6 +14,7 @@ import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.learn.growthcodelab.R;
+import com.learn.growthcodelab.playground.OverdrawFragment;
 import com.learn.growthcodelab.viewshowcase.constraint.AddViewToConstraintFragment;
 
 import java.util.concurrent.TimeUnit;
@@ -22,6 +23,10 @@ import java.util.concurrent.TimeUnit;
 public class PlayGroundActivity extends AppCompatActivity {
 
      private Handler frameMetricsHandler = new Handler();
+
+    private long all = 0;
+    private int overSixteen = 0;
+    private int totalFrames = 0;
 
     @RequiresApi(Build.VERSION_CODES.N)
     private  Window.OnFrameMetricsAvailableListener onFrameMetricsAvailableListener = new Window.OnFrameMetricsAvailableListener() {
@@ -32,6 +37,16 @@ public class PlayGroundActivity extends AppCompatActivity {
             long totalDuration = frameMetricsCopy.getMetric(FrameMetrics.TOTAL_DURATION);
             Log.d("Metrics", "LAYOUT_MEASURE_DURATION:" + TimeUnit.NANOSECONDS.toMillis(measureDuration));
             Log.d("Metrics", "TOTAL_DURATION:" + TimeUnit.NANOSECONDS.toMillis(totalDuration));
+            all += totalDuration;
+            totalFrames++;
+            Log.d("Metrics", "LAYOUT_MEASURE_DURATION:" + TimeUnit.NANOSECONDS.toMillis(measureDuration));
+            Log.d("Metrics", "TOTAL_DURATION:" + TimeUnit.NANOSECONDS.toMillis(totalDuration));
+            if (TimeUnit.NANOSECONDS.toMillis(totalDuration) > 16) {
+                overSixteen++;
+            }
+            Log.d("Metrics", "All:" + TimeUnit.NANOSECONDS.toMillis(all));
+            Log.d("Metrics", "totalFrames:" + totalFrames);
+            Log.d("Metrics", "overSixteen:" + overSixteen);
         }
     };
 
@@ -41,12 +56,13 @@ public class PlayGroundActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
+        addOnFrameMetricsAvailableListener();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_play_ground);
         getSupportFragmentManager()
                 .beginTransaction()
                 .add(R.id.fl_play_ground_container,
-                        AddViewToConstraintFragment.Companion.newInstance(),
+                        OverdrawFragment.Companion.newInstance(),
                         "add view to constraint")
                 .addToBackStack(null)
                 .commit();
