a168e6955aa5aff5090dd28cce3d637fc9471f12
Jerry Li <jerli@xogrp.com>
2019-06-16 22:45:02 +0800

    Added article life cycle awareness

--- app/src/main/kotlin/com/learn/growthcodelab/jetpack/livedata/shared/ArticleListFragment.kt
+++ app/src/main/kotlin/com/learn/growthcodelab/jetpack/livedata/shared/ArticleListFragment.kt
@@ -1,6 +1,5 @@
 package com.learn.growthcodelab.jetpack.livedata.shared
 
-import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -13,23 +12,14 @@ import com.learn.growthcodelab.fragment.BaseFragment
 class ArticleListFragment : BaseFragment() {
 
 
-    private lateinit var articleNavigator: ArticleNavigator
-
-    override fun onAttach(context: Context?) {
-        super.onAttach(context)
-        articleNavigator = if (context is ArticleNavigator) context
-        else {
-            throw IllegalStateException("The host Activity should implement the ArticleNavigator")
-        }
-    }
+    private val articleLifeCycleAwareness: ArticleLifeCycleAwareness = ArticleLifeCycleAwareness()
 
     override fun getLayoutRes(): Int {
         return R.layout.fragment_article_list
     }
 
-    override fun bindView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
-        return DataBindingUtil.inflate<FragmentArticleListBinding>(inflater, layoutRes, container, false)
-                .apply { sharedViewModel = articleNavigator.obtainArticleSharedViewModel() }
-                .root
-    }
+    override fun bindView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View =
+            DataBindingUtil.inflate<FragmentArticleListBinding>(inflater, layoutRes, container, false)
+                    .apply { sharedViewModel = articleLifeCycleAwareness.articleNavigator.obtainArticleSharedViewModel() }
+                    .root
 }
