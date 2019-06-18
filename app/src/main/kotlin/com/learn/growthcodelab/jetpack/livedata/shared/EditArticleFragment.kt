d123c1e7ac05d5c09f4b86523f104f66276213a0
Jerry Li <xiaoroyg@163.com>
2019-06-18 23:49:57 +0800

    Finished navigate to edit article.

--- app/src/main/kotlin/com/learn/growthcodelab/jetpack/livedata/shared/EditArticleFragment.kt
+++ app/src/main/kotlin/com/learn/growthcodelab/jetpack/livedata/shared/EditArticleFragment.kt
@@ -1,5 +1,6 @@
 package com.learn.growthcodelab.jetpack.livedata.shared
 
+import android.os.Bundle
 import com.learn.growthcodelab.R
 import com.learn.growthcodelab.fragment.BaseFragment
 
@@ -7,4 +8,15 @@ class EditArticleFragment : BaseFragment() {
 
 
     override fun getLayoutRes(): Int = R.layout.fragment_edit_article
+
+    companion object {
+        private const val BUNDLE_KEY_TITLE = "bundle_key_title"
+
+        fun newInstance(title: String) =
+                EditArticleFragment().apply {
+                    arguments = Bundle().apply {
+                        putString(BUNDLE_KEY_TITLE, title)
+                    }
+                }
+    }
 }
