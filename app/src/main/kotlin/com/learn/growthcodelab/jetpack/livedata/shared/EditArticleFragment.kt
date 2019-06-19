4676d84efeeef60269f68ecc4b17a175da13f9a3
Jerry Li <xiaoroyg@163.com>
2019-06-19 23:53:54 +0800

    Used life cycle awarenness to obtain shared view model.

--- app/src/main/kotlin/com/learn/growthcodelab/jetpack/livedata/shared/EditArticleFragment.kt
+++ app/src/main/kotlin/com/learn/growthcodelab/jetpack/livedata/shared/EditArticleFragment.kt
@@ -1,21 +1,40 @@
 package com.learn.growthcodelab.jetpack.livedata.shared
 
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import com.learn.growthcodelab.R
+import com.learn.growthcodelab.databinding.FragmentEditArticleBinding
 import com.learn.growthcodelab.fragment.BaseFragment
 
 class EditArticleFragment : BaseFragment() {
 
+    private lateinit var title: String
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        title = arguments?.getString(BUNDLE_KEY_ARTICLE_TITLE)
+                ?: throw IllegalArgumentException("Should put he title into the bundle")
+    }
+
+    override fun bindView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        return DataBindingUtil.inflate<FragmentEditArticleBinding>(inflater, layoutRes, container, false)
+                .apply {  }.root
+    }
 
     override fun getLayoutRes(): Int = R.layout.fragment_edit_article
 
     companion object {
-        private const val BUNDLE_KEY_TITLE = "bundle_key_title"
+        private const val BUNDLE_KEY_ARTICLE_TITLE = "bundle_key_title"
 
         fun newInstance(title: String) =
                 EditArticleFragment().apply {
                     arguments = Bundle().apply {
-                        putString(BUNDLE_KEY_TITLE, title)
+                        putString(BUNDLE_KEY_ARTICLE_TITLE, title)
                     }
                 }
     }
---
+++ app/src/main/kotlin/com/learn/growthcodelab/utils/BundleConstructor.kt
@@ -0,0 +1,12 @@
+package com.learn.growthcodelab.utils
+
+import android.os.Bundle
+
+/*
+fun bundle(block: Bundle.() -> Unit): Bundle {
+
+}
+
+fun string(block: Bundle.(String) -> Unit) {
+
+}*/
