75e88657c8304c39484e3dd1f20e077b0981314e
Jerry Li <xiaoroyg@163.com>
2019-07-14 22:29:47 +0800

    Finished create search query change observable.

--- app/src/main/kotlin/com/learn/growthcodelab/rx/cheese/CheeseActivity.kt
+++ app/src/main/kotlin/com/learn/growthcodelab/rx/cheese/CheeseActivity.kt
@@ -1,7 +1,10 @@
 package com.learn.growthcodelab.rx.cheese
 
 import android.annotation.SuppressLint
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
@@ -10,10 +13,12 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.learn.growthcodelab.R
 import com.learn.growthcodelab.activity.BaseActivity
+import com.learn.growthcodelab.widget.SimpleTextWatcher
 import io.reactivex.Observable
 import io.reactivex.Scheduler
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
+import java.util.concurrent.TimeUnit
 
 class CheeseActivity : BaseActivity() {
 
@@ -31,8 +36,9 @@ class CheeseActivity : BaseActivity() {
         btnCheeseSearch = findViewById(R.id.btn_cheese_search)
         etCheeseSearch = findViewById(R.id.et_cheese_search)
         recyclerCheese = findViewById(R.id.recycler_cheese)
+        progressCheese = findViewById(R.id.progress_cheeses)
+        cheeseProvider = CheeseProvider()
         cheesesAdapter = CheesesAdapter()
-
         recyclerCheese.apply {
             layoutManager = LinearLayoutManager(context)
             adapter = cheesesAdapter
@@ -44,13 +50,16 @@ class CheeseActivity : BaseActivity() {
     @SuppressLint("CheckResult")
     override fun onStart() {
         super.onStart()
-        createSearchButtonClickObservable()
-                .subscribeOn(AndroidSchedulers.mainThread())
-                .observeOn(AndroidSchedulers.mainThread())
+        val searchButtonClick = createSearchButtonClickObservable()
+        val searchQueryChange = createSearchQueryChangeObservable()
+        Observable.merge<String>(searchButtonClick, searchQueryChange)
                 .doOnNext { showProgress(true) }
                 .observeOn(Schedulers.io())
                 .flatMap { cheeseProvider.search(it) }
+                .observeOn(AndroidSchedulers.mainThread())
                 .subscribe(this::showCheeseSearchResult)
+
+
     }
 
     private fun createSearchButtonClickObservable(): Observable<String> = Observable.create { emitter ->
@@ -58,6 +67,7 @@ class CheeseActivity : BaseActivity() {
             emitter.onNext(etCheeseSearch.text.toString())
         }
         emitter.setCancellable {
+            Log.d(TAG, "Click, setCancellable")
             btnCheeseSearch.setOnClickListener(null)
         }
     }
@@ -73,5 +83,28 @@ class CheeseActivity : BaseActivity() {
         progressCheese.visibility = visibility
     }
 
+    private fun createSearchQueryChangeObservable(): Observable<String> =
+            Observable.create<String> { emitter ->
+                val textWatcher = object : SimpleTextWatcher() {
+                    override fun onTextChanged(charSequence: CharSequence?, start: Int, before: Int, count: Int) {
+                        charSequence?.let { emitter.onNext(it.toString()) }
+                    }
+                }
+                etCheeseSearch.addTextChangedListener(textWatcher)
+                emitter.setCancellable {
+                    etCheeseSearch.removeTextChangedListener(textWatcher)
+                }
+            }
+                    .filter { it.length >= QUERY_MINIMUM_LENGTH }
+                    .debounce(1, TimeUnit.SECONDS)
+
+
+    companion object {
+        const val TAG = "cheese"
+        const val QUERY_MINIMUM_LENGTH = 2
+        fun start(context: Context) {
+            context.startActivity(Intent(context, CheeseActivity::class.java))
+        }
+    }
 
 }
--- app/src/main/kotlin/com/learn/growthcodelab/rx/cheese/Cheeses.kt
+++ app/src/main/kotlin/com/learn/growthcodelab/rx/cheese/Cheeses.kt
@@ -6,14 +6,14 @@ class Cheese(
 
     companion object {
         val cheeses = mapOf(
-                "A" to listOf(Cheese("A1-Cheese"), Cheese("A2-Cheese"), Cheese("A2-Cheese")),
-                "B" to listOf(Cheese("B1-Cheese"), Cheese("B2-Cheese"), Cheese("B3-Cheese"),
+                "AAA" to listOf(Cheese("A1-Cheese"), Cheese("A2-Cheese"), Cheese("A2-Cheese")),
+                "BBB" to listOf(Cheese("B1-Cheese"), Cheese("B2-Cheese"), Cheese("B3-Cheese"),
                         Cheese("B4-Cheese")),
-                "C" to listOf(Cheese("C1-Cheese"), Cheese("C2-Cheese"), Cheese("C3-Cheese"),
-                        Cheese("C4-Cheese"), Cheese("C5-Cheese")),
-                "O" to listOf(Cheese("Cheese-First"), Cheese("Cheese-Second"), Cheese("Cheese-Fourth"),
-                        Cheese("Cheese-Fifth"))
-        )
+                "CCC" to listOf(Cheese("C1-Cheese"), Cheese("C2-Cheese"), Cheese("C3-Cheese"),
+                        Cheese("C4-Cheese"), Cheese("C5-Cheese")))
+
+        val defaultCheese = listOf(Cheese("Cheese-First"), Cheese("Cheese-Second"), Cheese("Cheese-Fourth"),
+                Cheese("Cheese-Fifth"))
     }
 
 }
