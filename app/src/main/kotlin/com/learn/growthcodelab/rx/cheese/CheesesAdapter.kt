5cd84f7fd6311eeb274106314b89d02d80159deb
Jerry Li <xiaoroyg@163.com>
2019-07-20 12:06:23 +0800

    Finished like the cheese

--- app/src/main/kotlin/com/learn/growthcodelab/rx/cheese/CheesesAdapter.kt
+++ app/src/main/kotlin/com/learn/growthcodelab/rx/cheese/CheesesAdapter.kt
@@ -1,11 +1,34 @@
 package com.learn.growthcodelab.rx.cheese
 
+import android.view.ViewGroup
 import com.learn.growthcodelab.R
 import com.learn.growthcodelab.recycler.BaseDataBindingAdapter
+import com.learn.growthcodelab.recycler.DataBindingViewHolder
+import com.learn.growthcodelab.view.CheckableImageView
+import io.reactivex.BackpressureStrategy
+import io.reactivex.Observable
+import io.reactivex.android.schedulers.AndroidSchedulers
 
 class CheesesAdapter : BaseDataBindingAdapter() {
 
     private val cheeseList = mutableListOf<Cheese>()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DataBindingViewHolder {
+        return super.onCreateViewHolder(parent, viewType).apply {
+            Observable.create<Boolean> { emitter ->
+                val ivCheeseFavorite = this.binding.root.findViewById<CheckableImageView>(R.id.iv_cheese_favorite)
+                emitter.setCancellable { ivCheeseFavorite.setOnClickListener(null) }
+                ivCheeseFavorite.setOnClickListener {
+                    emitter.onNext(ivCheeseFavorite.isChecked)
+                }
+            }
+                    .toFlowable(BackpressureStrategy.LATEST)
+                    .subscribeOn(AndroidSchedulers.mainThread())
+                    .subscribe {
+                        cheeseList[adapterPosition].hasFavorite = it
+                    }
+        }
+    }
 
     override fun getItemByPosition(position: Int): Any = cheeseList[position]
 
