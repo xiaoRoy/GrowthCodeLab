34cb87230986a657a8592bc13bf2f2559ff10cc3
Jerry Li <jerli@xogrp.com>
2019-12-06 18:09:58 +0800

    Setup the navigation pages.

---
+++ app/src/main/kotlin/com/learn/growthcodelab/navigation/NavHomeFragment.kt
@@ -0,0 +1,11 @@
+package com.learn.growthcodelab.navigation
+
+import com.learn.growthcodelab.R
+import com.learn.growthcodelab.fragment.BaseFragment
+
+class NavHomeFragment : BaseFragment() {
+
+    override fun getLayoutRes(): Int {
+        return R.layout.fragment_nav_home
+    }
+}
---
+++ app/src/main/kotlin/com/learn/growthcodelab/navigation/NavigationActivity.kt
@@ -0,0 +1,6 @@
+package com.learn.growthcodelab.navigation
+
+import com.learn.growthcodelab.activity.BaseActivity
+
+class NavigationActivity : BaseActivity() {
+}
