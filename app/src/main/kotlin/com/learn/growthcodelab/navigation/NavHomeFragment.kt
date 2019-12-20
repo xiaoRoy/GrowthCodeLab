dae2397ed377f5c2317e3c18114a7afb3caab13b
Jerry Li <jerli@xogrp.com>
2019-12-20 12:10:55 +0800

    Setup navigation.

--- app/src/main/kotlin/com/learn/growthcodelab/navigation/NavHomeFragment.kt
+++ app/src/main/kotlin/com/learn/growthcodelab/navigation/NavHomeFragment.kt
@@ -1,5 +1,9 @@
 package com.learn.growthcodelab.navigation
 
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import androidx.navigation.fragment.findNavController
 import com.learn.growthcodelab.R
 import com.learn.growthcodelab.fragment.BaseFragment
 
@@ -7,5 +11,13 @@ class NavHomeFragment : BaseFragment() {
 
     override fun getLayoutRes(): Int {
         return R.layout.fragment_nav_home
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        view.findViewById<Button>(R.id.navigate_destination_button).setOnClickListener {
+            findNavController().navigate(R.id.action_flow_step_one)
+        }
+
     }
 }
