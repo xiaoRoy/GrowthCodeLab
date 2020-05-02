1c039cab39fa0493aace4efb5bc60fb9c1ef7442
Jerry Li <jerli@xogrp.com>
2020-05-02 22:54:00 +0800

    Learned navigation with action id and destination id.

--- app/src/main/kotlin/com/learn/growthcodelab/navigation/FlowStepFragment.kt
+++ app/src/main/kotlin/com/learn/growthcodelab/navigation/FlowStepFragment.kt
@@ -4,9 +4,15 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.learn.growthcodelab.R
 import com.learn.growthcodelab.fragment.BaseFragment
 
 class FlowStepFragment: BaseFragment() {
+
+    override fun getLayoutRes(): Int {
+        return R.layout.fragment_nav_flow_step_one
+    }
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return super.onCreateView(inflater, container, savedInstanceState)
     }
